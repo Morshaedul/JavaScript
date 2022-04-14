2004c945a02ac7c1142b02bc6115f5da82423e62
Morshaedul <morshaedulanirbanit@gmail.com>
2022-04-15 00:54:14 +0600

    first commit

---
+++ Js/index.js
@@ -0,0 +1,35 @@
+var myVar=document.querySelector("button");
+var add=document.querySelector("h1");
+
+function addStyle(){
+    add.innerHTML="MORSHAEDUL ALOM"
+    add.style.fontSize="5 rem"
+
+}
+function removeStyle(){
+    add.removestyle();
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var date =new Date();
+// var currentDate=date.getFullYear();
+// var Date=date.getDay();
+// document.write(currentDate);
+// document.write(date);
